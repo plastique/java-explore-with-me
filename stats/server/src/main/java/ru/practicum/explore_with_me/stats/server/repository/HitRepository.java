de07201f613eee0cd1b47a75b47d1466f0160375
Alexander Krutov <plastique.a@gmail.com>
2025-04-07 18:16:14 +0300

    Update 5

--- stats/server/src/main/java/ru/practicum/explore_with_me/stats/server/repository/HitRepository.java
+++ stats/server/src/main/java/ru/practicum/explore_with_me/stats/server/repository/HitRepository.java
@@ -18,18 +18,19 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
                     + "WHERE h.created_at >= :start AND h.created_at <= :end "
                     + "GROUP BY h.app, h.uri "
                     + "ORDER BY hits DESC",
-             nativeQuery = true
+            nativeQuery = true
     )
     List<StatsView> getStatsByDatesUnique(LocalDateTime start, LocalDateTime end);
 
     @Query(
-            value = "SELECT h.app, h.uri, CONCAT('1') hits "
+            value = "SELECT h.app, h.uri, COUNT(h.ip) hits "
                     + "FROM hits as h "
-                    + "WHERE h.created_at >= :start AND h.created_at <= :end",
+                    + "WHERE h.created_at >= :start AND h.created_at <= :end "
+                    + "GROUP BY h.app, h.uri "
+                    + "ORDER BY hits DESC",
             nativeQuery = true
     )
     List<StatsView> getStatsByDates(LocalDateTime start, LocalDateTime end);
-
 
     @Query(
             value = "SELECT h.app, h.uri, COUNT(DISTINCT h.ip) hits "
@@ -42,10 +43,13 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
     List<StatsView> getStatsByDatesAndUriUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query(
-            value = "SELECT h.app, h.uri, CONCAT('1') hits "
+            value = "SELECT h.app, h.uri, COUNT(h.ip) hits "
                     + "FROM hits as h "
-                    + "WHERE h.created_at >= :start AND h.created_at <= :end AND h.uri IN (:uris)",
+                    + "WHERE h.created_at >= :start AND h.created_at <= :end AND h.uri IN (:uris) "
+                    + "GROUP BY h.app, h.uri "
+                    + "ORDER BY hits DESC",
             nativeQuery = true
     )
     List<StatsView> getStatsByDatesAndUri(LocalDateTime start, LocalDateTime end, List<String> uris);
+
 }
