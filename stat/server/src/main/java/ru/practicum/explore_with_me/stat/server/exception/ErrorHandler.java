f639f6cabcf56cf04afc6d89da66be5eb1f63089
Alexander Krutov <plastique.a@gmail.com>
2025-04-21 13:37:39 +0300

    Main service: update 17

--- stat/server/src/main/java/ru/practicum/explore_with_me/stat/server/exception/ErrorHandler.java
+++ stat/server/src/main/java/ru/practicum/explore_with_me/stat/server/exception/ErrorHandler.java
@@ -3,13 +3,31 @@ package ru.practicum.explore_with_me.stat.server.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.security.InvalidParameterException;
 
 @RestControllerAdvice
 @Slf4j
 public class ErrorHandler {
+
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            InvalidParameterException.class,
+            MethodArgumentTypeMismatchException.class,
+            MissingServletRequestParameterException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleBadRequestException(Exception e) {
+        log.debug("BAD_REQUEST {}", e.getMessage(), e);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
